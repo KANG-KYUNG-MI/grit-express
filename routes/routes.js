962ebb75d600be377088de731e36b72ce11b7a9f
KANG-KYUNG-MI <112539881+KANG-KYUNG-MI@users.noreply.github.com>
2023-05-11 11:16:39 +0200

    Add files via upload

---
+++ routes/routes.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db_connection');
+const authMiddleware = require('./../middlewares/auth');
+
+
+router.get('/', (req, res) => {
+    const data = {
+        title: "Welcome",
+        message: req.flash('message')
+    }
+    console.log(data.message)
+    res.render('index', data)
+})
+
+router.get('/dashboard', authMiddleware, (req, res) => {
+    if (req.isAuthenticated) {
+        // If the user is authenticated
+        const username = req.session.username;
+        const data = {
+            name: username,
+            style: "color: red;",
+            message: req.flash('message')
+        }
+        res.render('dashboard', data)
+    } else {
+        res.redirect('/login');
+    }
+})
+
+router.get('/logout', (req, res) => {
+    if (req.session.authenticated && req.session.username) {
+        req.session.authenticated = false;
+        req.session.username = null;
+        req.flash('message', 'You are now logged out.');
+        res.redirect('/');
+    } else {
+        res.redirect('/login');
+    }
+})
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const sql = 'SELECT * FROM users WHERE email = ? AND password = ?';
+    const values = [email, password];
+    db.query(sql, values, function (error, results, fields) {
+        if (error) throw error;
+
+        if(results.length > 0){
+            req.session.username = results[0].name;
+            req.session.authenticated = true;
+            req.flash('message', 'You are now logged in.');
+            res.redirect('/dashboard');
+        }else{
+     
+           // res.render('login', req.flash('message', 'You aadasdre now logged out.'))
+
+           req.flash('message', 'Found no user, try it again!!!!!!')
+            res.redirect('/login');
+
+            //res.send('Found no users')
+        }
+
+    });
+
+})
+
+// SIGN UP
+router.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+// Route for creating a new user
+router.post('/users', (req, res) => {
+    const { name, email, password } = req.body;
+    const user = { name, email, password };
+
+    // Insert new user into MySQL database
+    db.query('INSERT INTO users SET ?', user, (err, results) => {
+        if (err) {
+            console.error('Error creating new user: ', err);
+            res.status(500).send('Error creating new user');
+            return;
+        }
+        console.log('New user created with id: ', results.insertId);
+        req.session.username = user.name;
+        req.session.authenticated = true;
+        res.redirect('/dashboard');
+    });
+});
+
+router.get('/profile', (req, res) => {
+    const sql = 'select * from users where name = ?';
+    const values = [req.session.username];
+
+    db.query(sql, values,  function (error, results, fields){
+
+        if(error) throw error;
+        if(results.length> 0) {
+
+            const data = {
+                name: results[0].name,
+                email: results[0].email, 
+            }
+            return res.render('profile', data);
+        }
+        return res.redirect('/login');
+    })
+})
+
+router.get('/api/statistics', (req, res, next)=>{
+    const sql = 'SELECT (SELECT COUNT(*) FROM users) As numOFusers, (SELECT COUNT(*) FROM posts) AS numOFPosts';
+
+    db.query(sql, function (error, results, fields){
+
+        if(error) throw error;
+        return res.status(200).json(results)
+    });
+
+})
+module.exports = router;
